f516cf230b28cf00222c85f114c17306a3f7160e
ViKto568 <ViTiAi568@gmail.com>
2025-06-22 18:27:13 +0300

    Завдання 9
    js\task-09
    Напиши скрипт, який змінює кольори фону елемента <body> через інлайн-стиль по кліку на button.change-color і виводить значення кольору в span.color.

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,11 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const body = document.querySelector('body');
+const colorButton = document.querySelector('.change-color');
+const colorOutput = document.querySelector('.color');
+colorButton.addEventListener('click', () => {
+  const randomColor = getRandomHexColor();
+  body.style.backgroundColor = randomColor;
+  colorOutput.textContent = randomColor;
+});
