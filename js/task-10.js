7fde20a05fbf201ea5fceb6b1d84a3e84f317d55
ViKto568 <ViTiAi568@gmail.com>
2025-06-22 21:17:54 +0300

    Завдання 10
    js\task-10.js
    Створи функцію createBoxes(amount), яка приймає один параметр - число. Функція створює стільки <div>, скільки вказано в amount і додає їх у div#boxes.
    
    Розміри найпершого <div> - 30px на 30px.
    Кожен елемент після першого повинен бути ширшим і вищим від попереднього на 10px.
    Всі елементи повинні мати випадковий колір фону у форматі HEX. Використовуй готову функцію getRandomHexColor для отримання кольору.

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,40 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const input = document.querySelector('#controls input');
+const createBtn = document.querySelector('button[data-create]');
+const destroyBtn = document.querySelector('button[data-destroy]');
+const boxesContainer = document.querySelector('#boxes');
+function createBoxes(amount) {
+  const boxes = [];
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement('div');
+    const size = 30 + i * 10;
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxes.push(box);
+  }
+  boxesContainer.append(...boxes);
+}
+function destroyBoxes() {
+  boxesContainer.innerHTML = '';
+}
+createBtn.addEventListener('click', () => {
+  const amount = Number(input.value);
+  if (amount > 0) {
+    createBoxes(amount);
+  }
+}
+);
+destroyBtn.addEventListener('click', destroyBoxes);
+input.addEventListener('input', () => {
+  const amount = Number(input.value);
+  if (amount < 1) {
+    input.value = '';
+  }
+});
+
+
+
+  
